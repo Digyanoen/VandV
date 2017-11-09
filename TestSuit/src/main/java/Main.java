81a5ab6857e3b1d660fa1be055b6a146039d6c59
Julien Laurent <digyanoen@orange.fr>
2017-11-09 12:37:06 +0100

    new commit

--- TestSuit/src/main/java/Main.java
+++ TestSuit/src/main/java/Main.java
@@ -13,64 +13,79 @@ import spoon.reflect.visitor.Filter;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 import javax.tools.ToolProvider;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.Assert.assertTrue;
 
 public class Main {
     public static void main(String[] args){
 
+        try {
+            Process p = Runtime.getRuntime().exec("ls");
+            printLines("test", p.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //("/home/julien/Documents/TP/VV/DummyProject/src/test/java/");
 
 
-        Launcher launcher = new Launcher();
-        launcher.getEnvironment().setAutoImports(true);
-        launcher.getEnvironment().setNoClasspath(true);
-
-        File inDir = new File("/home/julien/Documents/TP/VV/DummyProject/src");
-
-        launcher.addInputResource(inDir.getPath());
-        launcher.setSourceOutputDirectory(new File("/home/julien/Documents/TP/VV/VandV/TestSuit/ressources"));
-        launcher.addProcessor(new MyProcess());
-        launcher.buildModel();
-        launcher.prettyprint();
-        CtModel root = launcher.getModel();
-
-        for (CtMethod<?> meth : root.getRootPackage().getElements(new TypeFilter<CtMethod>(CtMethod.class) {
-            @Override
-            public boolean matches(CtMethod element) {
-                return super.matches(element) && element.getAnnotation(Test.class) == null;
-            }
-        })) {
-            meth.getBody().forEach((CtStatement ac) -> {
-                ac.getFactory();
-            });
-            //assertTrue("naming contract violated for "+meth.getParent(CtClass.class).getSimpleName(), meth.getParent(CtClass.class).getSimpleName().startsWith("Test") || meth.getParent(CtClass.class).getSimpleName().endsWith("Test"));
-        }
-
-        Result r = new JUnitCore().run();
-
-//        root.getAllPackages().stream().forEach(
-//                p->System.out.println("p: " + p.getQualifiedName())
-//        );
-
-//        System.out.println();
+//        Launcher launcher = new Launcher();
+//        launcher.getEnvironment().setAutoImports(true);
+//        launcher.getEnvironment().setNoClasspath(true);
 //
-//        root.getAllTypes().stream().forEach(
-//                s->System.out.println("s: " + s.getQualifiedName())
-//        );
+//        File inDir = new File("/home/julien/Documents/TP/VV/DummyProject/src/test");
+//
+//        launcher.addInputResource(inDir.getPath());
+//        launcher.setSourceOutputDirectory(new File("/home/julien/Documents/TP/VV/VandV/TestSuit/ressources"));
+////        launcher.addProcessor(new MyProcess());
+////        launcher.buildModel();
+////        launcher.prettyprint();
+//        launcher.run();
+//        CtModel root = launcher.getModel();
+//
+////        for (CtMethod<?> meth : root.getRootPackage().getElements(new TypeFilter<CtMethod>(CtMethod.class) {
+////            @Override
+////            public boolean matches(CtMethod element) {
+////                return super.matches(element) && element.getAnnotation(Test.class) == null;
+////            }
+////        })) {
+////            meth.getBody().forEach((CtStatement ac) -> {
+////                ac.getFactory();
+////            });
+////            //assertTrue("naming contract violated for "+meth.getParent(CtClass.class).getSimpleName(), meth.getParent(CtClass.class).getSimpleName().startsWith("Test") || meth.getParent(CtClass.class).getSimpleName().endsWith("Test"));
+////        }
+//
+////        Result r = new JUnitCore().run();
+//
+////        root.getAllPackages().stream().forEach(
+////                p->System.out.println("p: " + p.getQualifiedName())
+////        );
+//
+////        System.out.println();
+////
+////        root.getAllTypes().stream().forEach(
+////                s->System.out.println("s: " + s.getQualifiedName())
+////        );
+//
+//
+//// list all classes of the model
+////        for(CtType<?> s : root.getAllTypes()) {
+////            System.out.println("class: "+s.getQualifiedName());
+////        }
+//
+//        System.out.println("Hello World!");
+    }
 
-
-// list all classes of the model
-//        for(CtType<?> s : root.getAllTypes()) {
-//            System.out.println("class: "+s.getQualifiedName());
-//        }
-
-        System.out.println("Hello World!");
+    private static void printLines(String name, InputStream ins) throws Exception {
+        String line = null;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(ins));
+        while ((line = in.readLine()) != null) {
+            System.out.println(name + " " + line);
+        }
     }
 
 //    @Test
