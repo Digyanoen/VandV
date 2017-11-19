ee8d5c684466df403f55ca0bb2d433c49e2d0506
Nicolas Fortun <nicolas.fortun@etudiant.univ-rennes1.fr>
2017-11-19 21:52:23 +0100

    Test for the MethodVoidProcessor

--- TestSuit/src/main/java/processors/MethodVoidProcessor.java
+++ TestSuit/src/main/java/processors/MethodVoidProcessor.java
@@ -1,26 +1,42 @@
 package processors;
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtBlock;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class MethodVoidProcessor extends AbstractProcessor<CtClass> {
 
-
+    List <CtClass> ctClasses;
     @Override
     public void process(CtClass ctClass) {
-       Set ctMethods = ctClass.getMethods();
+        ctClasses = new ArrayList<>();
+        CtClass ctClassCloned = ctClass.clone();
+        ctClasses.add(ctClass);
 
-       ctMethods.stream().filter(m -> ((CtMethod)m).getType().getSimpleName().equals("void") )
+       ctClassCloned.getMethods().stream().filter(m -> ((CtMethod)m).getType().getSimpleName().equals("void") )
                .forEach(
-                       method -> ((CtMethod)method).setBody(null));
+                       method ->
+                       {
+                           CtBlock body = ((CtMethod)method).getBody();
+                           ((CtMethod)method).setBody(null);
+                           ctClasses.add(ctClassCloned.clone());
+                           ((CtMethod) method).setBody(body);
+                       });
 
     }
 
+    public List<CtClass> getCtClasses() {
+        return ctClasses;
+    }
 
+    public void setCtClasses(List<CtClass> ctClasses) {
+        this.ctClasses = ctClasses;
+    }
 }
--- TestSuit/src/test/java/processor/MethodVoidProcessorTest.java
+++ TestSuit/src/test/java/processor/MethodVoidProcessorTest.java
@@ -2,14 +2,14 @@ package processor;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import processors.MethodVoidProcessor;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtReturn;
 import spoon.reflect.code.CtStatement;
-import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.declaration.*;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.support.reflect.code.CtBlockImpl;
 import spoon.support.reflect.code.CtLiteralImpl;
@@ -18,25 +18,43 @@ import spoon.support.reflect.declaration.CtClassImpl;
 import spoon.support.reflect.declaration.CtMethodImpl;
 import spoon.support.reflect.reference.CtTypeReferenceImpl;
 
-public class MethodVoidProcessorTest {
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class MethodVoidProcessorTest{
 
 
-    private MethodVoidProcessor methodVoidProcessor;
+    private static MethodVoidProcessor methodVoidProcessor;
+    private static CtClass removeClass;
 
-    @Before
-    public void init(){
-        this.methodVoidProcessor = new MethodVoidProcessor();
-    }
+    @BeforeClass
+    public static void setUp(){
+        removeClass = new CtClassImpl();
+        methodVoidProcessor = new MethodVoidProcessor();
+        removeClass.setSimpleName("RemoveBody");
+        removeClass.addModifier(ModifierKind.PUBLIC);
 
-    @Test
-    public void removeBodyTest(){
-        CtClass removeBody = new CtClassImpl();
-        removeBody.setSimpleName("RemoveBody");
-        removeBody.addModifier(ModifierKind.PUBLIC);
+        CtMethod ctMethodNotVoid = new CtMethodImpl();
+        CtBlock ctBlockNotVoid = new CtBlockImpl();
+        CtStatement ctStatementNotVoid = removeClass.getFactory().createCodeSnippetStatement("System.out.println(\"body of the method\")");
+        CtReturn returnStatement = new CtReturnImpl<Integer>();
+
+        returnStatement.setReturnedExpression(new CtLiteralImpl().setValue(2));
+        ctBlockNotVoid.addStatement(ctStatementNotVoid);
+        ctBlockNotVoid.addStatement(returnStatement);
+        ctMethodNotVoid.setBody(ctBlockNotVoid);
+        ctMethodNotVoid.setSimpleName("bodyNotRemovedMethod");
+
+        ctMethodNotVoid.addModifier(ModifierKind.PUBLIC);
+        CtTypeReference ctTypeReferenceNotVoid = new CtTypeReferenceImpl();
+        ctTypeReferenceNotVoid.setSimpleName("int");
+        ctMethodNotVoid.setType(ctTypeReferenceNotVoid);
 
         CtMethod ctMethod = new CtMethodImpl();
         CtBlock ctBlock = new CtBlockImpl();
-        CtStatement ctStatement = removeBody.getFactory().createCodeSnippetStatement("System.out.println(\"body of the method\")");
+        CtStatement ctStatement = removeClass.getFactory().createCodeSnippetStatement("System.out.println(\"body of the method\")");
 
         ctBlock.addStatement(ctStatement);
         ctMethod.setBody(ctBlock);
@@ -46,40 +64,30 @@ public class MethodVoidProcessorTest {
         CtTypeReference ctTypeReference = new CtTypeReferenceImpl();
         ctTypeReference.setSimpleName("void");
         ctMethod.setType(ctTypeReference);
-        removeBody.addMethod(ctMethod);
+        List<CtTypeMember> methods = new LinkedList<>();
+        methods.add(ctMethod);
+        methods.add(ctMethodNotVoid);
+        removeClass.setTypeMembers(methods);
 
-        methodVoidProcessor.process(removeBody);
-
-        Assert.assertTrue("Body must be null", removeBody.getMethod("removeBodyMethod").getBody() == null);
 
     }
 
     @Test
-    public void bodyNotRemovedTest(){
-        CtClass removeBody = new CtClassImpl();
-        removeBody.setSimpleName("RemoveBody");
-        removeBody.addModifier(ModifierKind.PUBLIC);
+    public void removeBodyTest(){
+        methodVoidProcessor.process(removeClass);
+        for (CtClass c : methodVoidProcessor.getCtClasses().subList(1, methodVoidProcessor.getCtClasses().size())){
+            Assert.assertTrue("Body must be null", c.getMethod("removeBodyMethod").getBody() == null);
+        }
 
-        CtMethod ctMethod = new CtMethodImpl();
-        CtBlock ctBlock = new CtBlockImpl();
-        CtStatement ctStatement = removeBody.getFactory().createCodeSnippetStatement("System.out.println(\"body of the method\")");
-        CtReturn returnStatement = new CtReturnImpl<Integer>();
+    }
 
-        returnStatement.setReturnedExpression(new CtLiteralImpl().setValue(2));
-        ctBlock.addStatement(ctStatement);
-        ctBlock.addStatement(returnStatement);
-        ctMethod.setBody(ctBlock);
-        ctMethod.setSimpleName("bodyNotRemovedMethod");
+    @Test
+    public void bodyNotRemovedTest() {
 
-        ctMethod.addModifier(ModifierKind.PUBLIC);
-        CtTypeReference ctTypeReference = new CtTypeReferenceImpl();
-        ctTypeReference.setSimpleName("int");
-        ctMethod.setType(ctTypeReference);
-        removeBody.addMethod(ctMethod);
+        methodVoidProcessor.process(removeClass);
+        for (CtClass c : methodVoidProcessor.getCtClasses()) {
+            Assert.assertTrue("Body must not be null", c.getMethod("bodyNotRemovedMethod").getBody() != null);
 
-        methodVoidProcessor.process(removeBody);
-
-        Assert.assertTrue("Body must not be null", removeBody.getMethod("bodyNotRemovedMethod").getBody() != null);
-
+        }
     }
 }
