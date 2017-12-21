bc9276a30f12a81875e449eb4f57817a4f3810bb
Nicolas Fortun <nicolas.fortun@etudiant.univ-rennes1.fr>
2017-12-21 15:23:08 +0100

    Revert "Ajout de l'annotation @CoverageIgnore + tests pour le UnitTestHandler"
    This reverts commit b42fc60e15dda30eebb5ba188e535c09a4183b73.

--- TestSuit/src/main/java/Testing/TestUnitHandler.java
+++ TestSuit/src/main/java/Testing/TestUnitHandler.java
@@ -1,20 +1,11 @@
 package Testing;
 
 import com.sun.org.apache.xalan.internal.xsltc.compiler.CompilerException;
-import com.sun.tools.javac.main.JavaCompiler;
-import com.sun.tools.javac.util.Context;
 import net.sourceforge.cobertura.CoverageIgnore;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.notification.Failure;
 import spoon.Launcher;
-import spoon.compiler.Environment;
-import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtType;
-import spoon.reflect.factory.Factory;
-import spoon.reflect.visitor.DefaultJavaPrettyPrinter;
-import spoon.support.JavaOutputProcessor;
 
-import javax.tools.JavaFileObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -28,58 +19,50 @@ import java.util.List;
 public class TestUnitHandler {
 
     private static JUnitCore junit = new JUnitCore();
+    private static ClassLoader classLoader;
     public static File dest = new File("dest/"); //Dossier de destination
+    private static Launcher launcher;
     private static List<Class<?>> clazzes = new ArrayList<>();
 
-    private static Launcher launcher;
-    private static ClassLoader classLoader;
-
     /**
-     * Compile les sources du projet cible et
-     * récupère la liste des tests qui ont échoué
+     * Récupère la liste des tests qui ont échoué
      * @return La liste d'échecs
-     * @throws CompilerException La compilation a renvoyé une erreur
      */
-    static List<Failure> getFailures() throws CompilerException {
+    public static List<Failure> getFailures() throws CompilerException {
 
-        //Compile le projet cible
         compile();
 
         List<Failure> result = new ArrayList<>();
 
-        //Lance les tests et récupère les tests échouant
+
+
         for (Class<?> clazz : clazzes) {
-            System.out.println("ajout de la classe de test "+clazz.getSimpleName());
-            List<Failure> listResult = junit.run(clazz).getFailures();
-            System.out.println("failures récupérées");
-            result.addAll(listResult);
+            result.addAll(junit.run(clazz).getFailures());
         }
-        System.out.println("return result");
 
         return result;
     }
 
-    /**
-     * Initialise TestUnitHandler
-     * @param l Launcher Spoon contenant le modèle à tester
-     */
+
     public static void initialize(Launcher l) {
         launcher = l;//Récupère le dossier des classes de tests
 
-        //Compile entièrement le projet cible
         completeCompile();
 
-        File classRoot = new File("dest/target/test-classes"); //Condition sine qua non
+        File classRoot = new File("dest/target/test-classes"); //TODO Confirmer le lieu de la compile Maven
+
+
 
         //Initialise le ClassLoader
         try {
-            if (classLoader == null) classLoader = URLClassLoader.newInstance(new URL[]{classRoot.toURI().toURL()});
-            //Charge les différents tests
-            for(String elm: getTests(classRoot)) {
+            classLoader = URLClassLoader.newInstance(new URL[]{classRoot.toURI().toURL()});
+            //Lance les différents tests
+            List<String> testList = getTests(classRoot);
+            for(String elm: testList) {
 
                 Class<?> cls = null;
                 try {
-                    cls = classLoader.loadClass(elm);
+                    cls = classLoader.loadClass(elm);//elm.getQualifiedName());
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -91,12 +74,15 @@ public class TestUnitHandler {
         }
     }
 
-    /**
-     * lance la commande "mvn compile" sur le projet cible, copié dans le dossier "dest"
-     * @throws CompilerException La commande a échoué
-     */
     @CoverageIgnore
     private static void compile() throws CompilerException {
+        try {
+            deleteFiles(new File("dest/src/main/java"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        launcher.prettyprint();
 
         String[]command ={"mvn","compile"};
         ProcessBuilder ps=new ProcessBuilder(command);
@@ -107,25 +93,15 @@ public class TestUnitHandler {
         try {
             process = ps.start();
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = in.readLine();
-            while(line != null){
-                System.out.println(line);
-                line = in.readLine();
-            }
             process.waitFor();
             in.close();
-            System.out.println("compile fini");
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             throw new CompilerException("Fail to compile");
         }
     }
 
-    /**
-     * Récupère les tests sous la forme acceptée par classLoader
-     * @param classRoot Objet File pointant sur le dossier contenant les .class des tests
-     * @return La liste des noms des classes sous la forme nom.de.package.classe
-     */
+
     @CoverageIgnore
     private static List<String> getTests(File classRoot) {
         List<String> res = new ArrayList<>();
@@ -141,18 +117,11 @@ public class TestUnitHandler {
         return res;
     }
 
-    /**
-     * Méthode auxiliaire de getTests(File classRoot)
-     * @param dir Répertoir ou fichier courant
-     * @param pack nom du package courant
-     * @return La liste des noms des classes sous la forme nom.de.package.classe contenu dans dir
-     */
     @CoverageIgnore
     private static List<String> getTests(File dir, String pack) {
         List<String> res = new ArrayList<>();
         String name = dir.getName();
 
-        //Si dir est un répertoire, appelle la méthode sur son contenu
         if(dir.isDirectory()){
             File[] files = dir.listFiles();
             if (files != null) {
@@ -161,19 +130,14 @@ public class TestUnitHandler {
                 }
             }
         }else if(dir.isFile() && name.endsWith(".class") && !name.contains("$")){
-            //Si dir est un fichier .class génère le nom de package
             res.add(pack + name.substring(0,name.length()-6));
         }
 
         return res;
     }
 
-    /**
-     * Supprime les documents donnés en paramêtre
-     * @param file Fichier ou répertoire à supprimer
-     * @throws IOException Le fichier ou le répertoire n'a pas pu être supprimé
-     */
-    public static void deleteFiles(File file) throws IOException {
+    @CoverageIgnore
+    private static void deleteFiles(File file) throws IOException {
         File [] children = file.listFiles();
         if(children != null) {
             for (File child : children) {
@@ -183,9 +147,9 @@ public class TestUnitHandler {
         if(!file.delete()) throw new IOException();
     }
 
-    /**
-     * Compile l'intégralité du projet cible
-     */
+
+
+
     @CoverageIgnore
     private static void completeCompile() {
 
@@ -200,32 +164,11 @@ public class TestUnitHandler {
         try {
             process = ps.start();
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = in.readLine();
-            while(line != null){
-                System.out.println(line);
-                line = in.readLine();
-            }
-
             process.waitFor();
             in.close();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Génère le fichier Spoon dans la copie du projet cible
-     * @param element Class Spoon à générer
-     */
-    public static void replace(CtClass element){
-        CtType type = element.getPosition().getCompilationUnit().getMainType();
-        Factory factory = type.getFactory();
-        Environment env = factory.getEnvironment();
-
-        JavaOutputProcessor processor = new JavaOutputProcessor(new File("dest/src/main/java"), new DefaultJavaPrettyPrinter(env));
-        processor.setFactory(factory);
-
-        processor.createJavaFile(type);
     }
 
     @CoverageIgnore
